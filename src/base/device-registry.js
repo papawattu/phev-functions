87b9a62a8e34f81926813575e76b332ea5af6849
Jamie Nuttall <jamie@wattu.com>
2018-10-28 20:45:05 +0000

    simple device registry working with tests

--- src/base/device-registry.js
+++ src/base/device-registry.js
@@ -2,9 +2,23 @@ class DeviceRegistry {
     constructor(deps) {
         //super()
         this.store = deps.store
+        this.jwt = deps.jwt
     }
-    async get(deviceId) {
-        return await this.store.get(deviceId)
+    async get(args) {
+        
+        const { deviceId, jwt } = args
+
+        const deviceExists = await this.store.has(deviceId)
+        if(!deviceExists) {
+            return undefined
+        }
+        
+        const validJWT = await this.jwt.verify(jwt)
+        if(validJWT) {
+            return await this.store.get(deviceId)
+        } else {
+            return Promise.reject({ error : 'invalid JWT'})
+        }
     }
 }
 
--- src/base/device-registry.spec.js
+++ src/base/device-registry.spec.js
@@ -7,16 +7,19 @@ import Cache from 'cache-base'
 const assert = chai.assert
 
 describe('Device Registry', () => {
-    let sandbox = null;
+    let sandbox = null
     let store = null 
-    
+    let jwt = { verify : () => null }
+    let jwtInvalid = { verify : () => null }
 
     beforeEach(() => {
         store = new Cache()
         
         sandbox = sinon.createSandbox();
 
-        //sandbox.stub(device,'get').resolves({ deviceId: '123' })
+        sandbox.stub(jwt,'verify').returns(true)
+        sandbox.stub(jwtInvalid,'verify').returns(false)
+        
         //sandbox.stub(deviceNoDevice,'get').resolves(null)
         //sandbox.stub(events,'subscribe').resolves(true)
 
@@ -30,12 +33,13 @@ describe('Device Registry', () => {
     })
     it('Should get device', async () => {
         const deps = {
+            jwt,
             store
         }
         
         const deviceRegistry = new DeviceRegistry(deps)
         
-        const device = deviceRegistry.get('123')
+        const device = deviceRegistry.get({ deviceId : '123', jwt : 'xxxx' })
         
         assert.isNotNull(device)
 
@@ -43,16 +47,48 @@ describe('Device Registry', () => {
     it('Should get device details', async () => {
     
         const deps = {
+            jwt,
             store
         }
     
-        store.set('123', { deviceId : '123' })
         const deviceRegistry = new DeviceRegistry(deps)
-    
-        const device = deviceRegistry.get('123')
+        deps.store.set('123', { deviceId : '123' })
         
-        console.log(JSON.stringify(device))
-        assert.deepEqual(device, { deviceId : '123' })
+        const device = await deviceRegistry.get({ deviceId : '123', jwt : 'xxxx' })
+        
+        assert.deepInclude(device, { deviceId : '123' })
+        
+    })
+    it('Should return undefined if not found', async () => {
+    
+        const deps = {
+            jwt,
+            store
+        }
+    
+        const deviceRegistry = new DeviceRegistry(deps)
+        
+        const device = await deviceRegistry.get({ deviceId : '123', jwt : 'xxxx' })
+        
+        assert.isUndefined(device)
+        
+    })
+    it('Should reject if invalid JWT', async () => {
+    
+        const deps = {
+            jwt : jwtInvalid,
+            store
+        }
+    
+        const deviceRegistry = new DeviceRegistry(deps)
+        
+        try {
+            await deviceRegistry.get({ deviceId : '123', jwt : 'yyyyy' })
+            throw new Error('Should not get here')
+        } catch (err) {
+            
+            assert.isNotNull(err)
+        }
         
     })
 })
